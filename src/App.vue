fd1e8cd1a39ef789c01318d06a0caba255d8f127
hachi1994 <lhc1092751647@163.com>
2020-09-30 16:12:25 +0800

    v0.0.1 vue3.x学习

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-09-30 14:59:25
+ * @LastEditTime: 2020-09-30 15:49:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3.x_repo\vuedemo1\src\App.vue
+-->
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1 v-on:click="text">4444</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent} from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  methods: {
+    text(): number {
+      let ratio = 0
+      if (window.devicePixelRatio !== undefined) {
+        ratio = window.devicePixelRatio;
+      } else if (
+        window.outerWidth !== undefined &&
+        window.innerWidth !== undefined
+      ) {
+        ratio = window.outerWidth / window.innerWidth;
+      }
+
+      if (ratio) {
+        ratio = Math.round(ratio * 100);
+      }
+      console.log(ratio,14);
+      return ratio;
+    },
+  },
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-09-30 14:59:25
+ * @LastEditTime: 2020-09-30 16:07:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3.x_repo\vuedemo1\src\components\HelloWorld.vue
+-->
+<template>
+  <div>
+    <div
+      @Click="selectGirlFun(index)"
+      v-for="(item, index) in girls"
+      v-bind:key="index"
+    >
+      {{ item }}
+    </div>
+    <div>你选择了【{{ selectGirl }}】为你服务</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+interface DataProps {
+  girls: string[];
+  selectGirl: string;
+  selectGirlFun: (index: number) => void;
+}
+export default defineComponent({
+  name: "HelloWorld",
+  setup() {
+    const data: DataProps = reactive({
+      girls: ["1", "2", "3"],
+      selectGirl: "",
+      selectGirlFun: function (index: number) {
+        data.selectGirl = data.girls[index];
+      },
+    });
+    const refData = toRefs(data);
+    return {
+      ...refData,
+    };
+  },
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
